0e3cec6730afc22ff1d0a6add80dd787ffa71f4d
Amit Ranan <ranan.amit@gmail.com>
2017-03-23 23:24:44 -0700

    added node server for heroku login, image error handling

---
+++ server.js
@@ -0,0 +1,32 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// Unsafely enable cors
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log(`API running at localhost:${port}`);
--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -5,6 +5,7 @@ import PureRenderMixin from 'react/lib/ReactComponentWithPureRenderMixin';
 var ResponsiveReactGridLayout = require('react-grid-layout').Responsive;
 ResponsiveReactGridLayout = WidthProvider(ResponsiveReactGridLayout);
 import _ from 'lodash';
+import axios from 'axios';
 import Card from '../Card/Card';
 
 import './card-container.scss';
@@ -18,7 +19,12 @@ class CardContainer extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      imageUrl: ''
+    }
+
     this.handleDeleteCard = this.handleDeleteCard.bind(this);
+    this.handleImageError = this.handleImageError.bind(this);
   }
 
   componentWillMount() {
@@ -30,14 +36,26 @@ class CardContainer extends Component {
     console.log('deleting');
   }
 
+  handleImageError() {
+    console.log('error on image');
+  }
+
   createElement(catAndFact) {
-    var i = catAndFact.add ? '+' : catAndFact.i;
     catAndFact.x = Math.floor(Math.random() * 3);
+
+    let imageUrl = '';
+    function handleImageError() {
+      // this.setState({
+      //   imageUrl: './img/cat-placeholder.jpg'
+      // });
+      imageUrl = './img/cat-placeholder.jpg';
+      console.log('image error');
+    }
 
     return (
       <div key={catAndFact.id} data-grid={catAndFact} className="card">
-      <i className="fa fa-arrows fa-2x" aria-hidden="true"></i>
-          <img src={catAndFact.url} />
+      <i className="fa fa-arrows" aria-hidden="true"></i>
+          <img src={catAndFact.url} onError={handleImageError} />
           <p>{catAndFact.fact}</p>
       </div>
     );
