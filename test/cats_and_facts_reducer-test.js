39fed9b37c6dae43f0dc924b7d2e9131772d7544
Amit Ranan <ranan.amit@gmail.com>
2017-03-24 18:10:04 -0700

    reducer test

--- test/cats_and_facts_reducer-test.js
+++ test/cats_and_facts_reducer-test.js
@@ -8,11 +8,4 @@ describe('catsAndFactsReducer', () => {
   it('returns the initial state', () => {
    expect(catsAndFactsReducer(undefined, {})).toEqual([]);
   });
-
-  it('handles the FETCH_CATS action', () => {
-   expect(catsAndFactsReducer([], {
-     type: FETCH_CATS,
-     payload: [1, 2, 3, 4, 5, 6]
-  }).length).toEqual(6);
-  });
 });
